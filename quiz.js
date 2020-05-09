aab643a69093d35855fe39bf347754669fc053ce
Coded-man <49336593+Coded-man@users.noreply.github.com>
2020-05-09 18:56:31 +0100

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -33,12 +33,12 @@ covidQuiz=[
     },
     {
         question: 'Is there a cure for COVID-19?' ,
-        options: ['Yes', 'No', 'Maybe'],
+        options: ['Yes', 'No', 'Maybe', 'Not sure'],
         answer: 1
     },
     {
         question: 'Exposing yourself to the sun or to temperatures higher than 25C degrees does not prevent COVID-19' ,
-        options: ['True', 'False', 'No Idea'],
+        options: ['True', 'False', 'No Idea', 'Not sure'],
         answer: 0
     }
 ]
